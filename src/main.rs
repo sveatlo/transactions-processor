1af6ca506955bdfcbbf8334db4b17a10f2d1fb95
Svätopluk Hanzel <svatoplukhanzel@pm.me>
2025-08-19 12:54:16 +0200

    fix: Write logs to stderr

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ const BUILD_TIMESTAMP: &str = env!("GIT_TIMESTAMP");
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt()
+        .with_writer(io::stderr)
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
